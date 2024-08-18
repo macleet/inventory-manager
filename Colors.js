340b7406bdf383b091f6b3facde16c4617e4bc52
Mac Lee <seemin0506@gmail.com>
2024-08-18 15:47:32 -0700

    Add Colors.js

---
+++ Colors.js
@@ -0,0 +1,13 @@
+export default {
+  background: {
+    primary:    '#F8F8FF',
+    secondary: '#22B2',
+    tertiary:  '#E5E5F490',
+  },
+  primary:      '#229C',
+  inactiveTab:  '#D5D5F4',
+  activeTab:    '#D5D5D5',
+  text: {
+    darkGray:   '#77B'
+  }
+};
